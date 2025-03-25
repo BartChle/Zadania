ff7188aad194f04b8c4e05ccb3eb0962bccdafdc
BartChle <b.chlebowski@student.uw.edu.pl>
2025-03-25 16:02:37 +0100

    Add files via upload

---
+++ Zajęcia 1/Zadanie 1i3.R
@@ -0,0 +1,30 @@
+#Praca domowa z pierwszych zajec
+
+# 1. Stwórz funkcję o nazwie kostka, która będzie symulować n rzutów kostką.
+
+
+kostka = function(n){
+  
+  rzuty = sample.int(6,size = n, replace = TRUE)
+  
+  return(rzuty)
+  
+}
+
+
+kostka(5)
+
+# 3. Stwórz funkcję o nazwie pole_kola, która oblicza pole powierzchni koła dla danego promienia.
+
+pole_kola=function(r){
+  
+  if(r <= 0) {
+    stop("Promien musi być liczba dodatnia!")
+  }
+  
+  pole = pi*r^2
+  return(pole)
+  
+}
+
+pole_kola(6)
